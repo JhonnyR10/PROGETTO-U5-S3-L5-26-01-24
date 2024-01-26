a33fe9a913b744598619bcbfb07aed9fbe91fa00
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-26 09:32:26 +0100

    Creazione progetto

---
+++ src/main/java/giovannilongo/PROGETTOU5S3L5260124/ProgettoU5S3L5260124Application.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTOU5S3L5260124;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5S3L5260124Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5S3L5260124Application.class, args);
+	}
+
+}
---
+++ src/test/java/giovannilongo/PROGETTOU5S3L5260124/ProgettoU5S3L5260124ApplicationTests.java
@@ -0,0 +1,13 @@
+package giovannilongo.PROGETTOU5S3L5260124;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU5S3L5260124ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
