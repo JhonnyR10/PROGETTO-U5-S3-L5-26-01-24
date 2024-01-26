5a4cad3ab025f8e368b42ede4d77339f29b6777e
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-26 13:10:50 +0100

    add exceptions

---
+++ src/main/java/giovannilongo/PROGETTOU5S3L5260124/exceptions/ExceptionsHandler.java
@@ -0,0 +1,54 @@
+package giovannilongo.PROGETTOU5S3L5260124.exceptions;
+
+import giovannilongo.PROGETTOU5S3L5260124.payloads.ErrorsPayloadWithList;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestControllerAdvice
+@Slf4j
+public class ExceptionsHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsPayloadWithList handleBadRequest(BadRequestException e) {
+        List<String> errorsMessages = new ArrayList<>();
+        if (e.getErrorsList() != null)
+            errorsMessages = e.getErrorsList().stream().map(err -> err.getDefaultMessage()).toList();
+        return new ErrorsPayloadWithList(e.getMessage(), new Date(), errorsMessages);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED) // 401
+    public ErrorsPayload handleUnauthorized(UnauthorizedException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN) // 403
+    public ErrorsPayload handleAccessDenied(AccessDeniedException ex) {
+        return new ErrorsPayload("Il tuo ruolo non permette di accedere a questa funzionalità!", LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorsPayload handleNotFound(NotFoundException e) {
+        return new ErrorsPayload(e.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorsPayload handleGeneric(Exception e) {
+        e.printStackTrace();
+        return new ErrorsPayload("Errore generico, risolveremo il prima possibile", LocalDateTime.now());
+    }
+
+}
