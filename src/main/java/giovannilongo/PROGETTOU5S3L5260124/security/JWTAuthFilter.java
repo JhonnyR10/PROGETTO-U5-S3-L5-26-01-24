e81a3eba0dee3a5c21218e0ad5ac406606459369
Giovanni Longo <giovanni.longo10@icloud.com>
2024-01-26 13:01:29 +0100

    security

---
+++ src/main/java/giovannilongo/PROGETTOU5S3L5260124/security/JWTAuthFilter.java
@@ -0,0 +1,48 @@
+package giovannilongo.PROGETTOU5S3L5260124.security;
+
+import giovannilongo.PROGETTOU5S3L5260124.entities.Utente;
+import giovannilongo.PROGETTOU5S3L5260124.services.UtenteService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthFilter extends OncePerRequestFilter {
+    // Creo un filtro che andrò ad aggiungere alla Security Filter Chain
+
+    @Autowired
+    private JWTTools jwtTools;
+    @Autowired
+    private UtenteService utenteService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            throw new UnauthorizedException("Per favore metti il token nell'Authorization header");
+        } else {
+            String accessToken = authHeader.substring(7);
+            jwtTools.verifyToken(accessToken);
+            String id = jwtTools.extractIdFromToken(accessToken); // L'id è nel token quindi devo estrarlo da lì
+            Utente utente = utenteService.findById(Long.parseLong(id));
+            Authentication authentication = new UsernamePasswordAuthenticationToken(utente, null, utente.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            filterChain.doFilter(request, response); // va al prossimo elemento della catena
+        }
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        return new AntPathMatcher().match("/auth/**", request.getServletPath());
+    }
+}
---
+++ src/main/java/giovannilongo/PROGETTOU5S3L5260124/services/UtenteService.java
@@ -0,0 +1,4 @@
+package giovannilongo.PROGETTOU5S3L5260124.services;
+
+public class UtenteService {
+}
